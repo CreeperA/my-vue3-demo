b788788ab932d6b1fd28bf93bae3ce81a851c357
CreeperA <499198977@qq.com>
2021-09-23 16:48:58 +0800

    init element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import { key, store } from './store'
@@ -9,4 +11,5 @@ import './style/index.scss'
 createApp(App)
   .use(router)
   .use(store, key)
+  .use(ElementPlus)
   .mount('#app')
