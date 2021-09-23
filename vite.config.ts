7332f2421a316c270ae37c1ea798817eeab579c4
CreeperA <499198977@qq.com>
2021-09-23 14:52:12 +0800

    add environment variables & server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,22 @@ export default defineConfig({
         additionalData: '@import "@/style/variables.scss";' // 配置sass全局变量
       }
     }
+  },
+  server: {
+    proxy: {
+      // 字符串简写方法
+      '/foo': 'http://localhost:4567/foo',
+      // 选项写法
+      '/admin': {
+        target: 'https://shop.fed.lagou.com/api/admin', // 代理的目标地址
+        /**
+         * 代理服务是通过把 HTTP 的 requestHeader 的 origin 字段映射到
+         * 代理的目标地址,此时的 origin 是 localhost:3000,为 true 时代理
+         * 服务就会把 origin 修改为 目标地址 https://shop.fed.lagou.com/api
+         */
+        changeOrigin: true
+        // rewrite: (path) => path.replace(/^\/admin/, '')
+      }
+    }
   }
 })
